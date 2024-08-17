b6168b8fcfedaa74c797af8f26af8dd449029825
amit08255 <amitcute3@gmail.com>
2024-08-17 17:50:53 +0530

    Handle custom delimiter.

--- __tests__/index.js
+++ __tests__/index.js
@@ -9,8 +9,14 @@ describe('add should work correctly', () => {
     test('should add numbers from string correctly', () => {
         expect(add('1')).toBe(1);
         expect(add('1,4')).toBe(5);
-        expect(add('1,2,3')).toBe(6);
-        expect(add('1,2,3,4')).toBe(10);
+        expect(add('1,2,35')).toBe(38);
+        expect(add('1,250,3,4')).toBe(258);
         expect(add('1,2\n3,4,5\n6')).toBe(21);
+        expect(add('//;\n2;3;75;500')).toBe(580);
+        expect(add('//;\n1;2;3;4\n5;6')).toBe(21);
+    });
+
+    test('Should throw error when custom delimiter string format is invalid', () => {
+        expect(() => add('//;1;2;3;4;5;6')).toThrow();
     });
 });
--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const add = (number) => {
-    if (!number) {
+function parseNumbersWithDelimeter(input) {
+    const regex = /^\/\/(.+?)\n(.*)$/gms;
+
+    const match = input.match(regex);
+
+    if (match) {
+        const prefix = input.split('\n', 1)[0];
+        const delimiter = prefix.replace('//', '').trim();
+        const numbers = input.replace(prefix, '').trim();
+
+        return {
+            delimiter,
+            numbers,
+        };
+    }
+    throw new Error(`Invalid input format: ${input}`);
+}
+
+const add = (numString) => {
+    if (!numString) {
         return 0;
     }
 
-    return number.split(/,|\n/)
+    const parsedNumber = numString.startsWith('//') ? parseNumbersWithDelimeter(numString) : null;
+    const number = parsedNumber ? parsedNumber.numbers : numString;
+    const delimiter = parsedNumber ? RegExp(`${parsedNumber.delimiter}|\n`) : /,|\n/;
+
+    return number.split(delimiter)
         .map((num) => parseInt(num.trim(), 10))
         .reduce((prev, current) => prev + current, 0);
 };
